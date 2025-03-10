ded9f3949fd8ea529481edbda8cb308e4ea3e36d
wycre <djones@wycre.net>
2025-03-10 15:23:51 -0600

    Add settings page

--- main.js
+++ main.js
@@ -1,20 +1,38 @@
 // requires the electron module
 const { app, BrowserWindow, ipcMain } = require("electron");
 
+const Store = require("electron-store");
+
 let mainWindow, subscriberWindow;
 
 function handleMessageReceived(event, message) {
     mainWindow.webContents.send('message-received', {data: message});
 }
 
+function restartSubscriber(event, data) {
+    subscriberWindow.webContents.reloadIgnoringCache();
+}
+
 // Start app
 app.whenReady().then(() => {
+    Store.initRenderer();
+
+    const store = new Store({
+        defaults: {
+            bouncerAddress: "http://cryptseek.wycre.net:9090/upload",
+            gatewayAddress: "tcp://cryptseek.wycre.net:5555",
+        }
+    });
+
+    console.log(app.getPath('userData'));
+
     // Register event listener for message reception
     ipcMain.on('message-received', handleMessageReceived)
+    ipcMain.on('settings-saved', restartSubscriber);
 
     // Create subscriber window
     subscriberWindow = new BrowserWindow({
-        show: false,
+        show: true,
         webPreferences: {
             nodeIntegration: true,
             contextIsolation: false
